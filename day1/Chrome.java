9c605cf2436086e279e247bb754849e44b5a4d56
gopalakrishnan46 <gpl.krishnan91@gmail.com>
2024-06-24 20:10:38 +0530

    Add files via upload
    Day1_Home Assignment

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package homeassignment.day1;
+
+public class Chrome {
+       
+	
+      public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey ='C';
+		
+		System.out.println("Chrome Home Page" + " "+ developer);
+		System.out.println("Chrome version is" +" "+ version);
+		System.out.println("Chrome is beta" +" "+ isBeta);
+		System.out.println("Chrome Release Year is" +" "+ releaseYear);
+		System.out.println("Chrome shortcut Key is" +" "+ shortcutKey);
+	}
+}
